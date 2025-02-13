0de75b53e55c9619c4c715786babab991828dc0c
Wisam <wisam.11w@hotmail.com>
2025-02-13 16:09:21 +0200

    weekly limit validation for each user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,43 +1,50 @@
 // models/User.js
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    profilePicture: {
+      type: String,
+      default: null,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "manager"],
+      default: "user",
+    },
+    verificationCode: {
+      type: String,
+      default: null,
+    },
+    verificationExpires: {
+      type: Date,
+      default: null,
+    },
+    emailChangeRequest: {
+      newEmail: { type: String, default: null },
+      verificationCode: { type: String, default: null },
+      expiresAt: { type: Date, default: null },
+    },
+    weeklyLimit: {
+      type: Number,
+      default: 0,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  name: {
-    type: String,
-    default: ''
-  },
-  profilePicture: {
-    type: String,
-    default: null
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin", "manager"], 
-    default: 'user'
-  },
-  verificationCode: {
-    type: String,
-    default: null
-  },
-  verificationExpires: {
-    type: Date,
-    default: null
-  },
-  emailChangeRequest: {
-    newEmail: { type: String, default: null },
-    verificationCode: { type: String, default: null },
-    expiresAt: { type: Date, default: null }
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
